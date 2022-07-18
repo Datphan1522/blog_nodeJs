a81abe42e969b3f35a832b84eac9ebe39069ab37
Datphan1522 <datphan1522@gmail.com>
2022-07-18 08:50:05 +0700

    update courses detail page

--- src/util/mongoose.js
+++ src/util/mongoose.js
@@ -1,8 +1,10 @@
 module.exports = {
-    //hàm xử lý array
+    //hàm xử lý một list
     mutipleMongooseToObject: function(mongooses) {
         return mongooses.map(mongoose => mongoose.toObject())
     },
+    //hàm xử lý một đối tượng
+
     mongooseToObject: function(mongoose) {
         return mongoose ? mongoose.toObject() : mongoose
     }
